e71090a8973597a0505d417b85cec67a0ad0f5c6
unknown <nicolassakr2001@outlook.com>
2022-05-04 11:32:37 +0300

    SignUpBug

--- app/src/main/java/com/lau/watched/SignUpPage.java
+++ app/src/main/java/com/lau/watched/SignUpPage.java
@@ -5,19 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpPage extends AppCompatActivity {
+
+    EditText pass;
+    EditText verifyPass;
+    TextView res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        pass = (EditText) findViewById(R.id.Password);
+        verifyPass = (EditText) findViewById(R.id.VerifyPass);
+        res = (TextView) findViewById(R.id.errorText)
     }
     public void btnOnClick(View v){
-        //add the information to the database
+        if(verifyPass.toString() == pass.toString()){
+            //add the information to the database
 
-        //login to the next page
-        Intent intent = new Intent(getApplicationContext(), Home.class);
-        startActivity(intent);
+            //login to the next page
+            Intent intent = new Intent(getApplicationContext(), Home.class);
+            startActivity(intent);
+        }
+        else{
+            res.setText("Passwords Don't match");
+        }
     }
 }
